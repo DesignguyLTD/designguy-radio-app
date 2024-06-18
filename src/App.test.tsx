b704868387d801bfc660800e1b1c440388a72899
seyifunmi Odediran <philipoluseyi@gmail.com>
2024-06-18 15:32:50 +0100

    local

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
+import {act} from 'react';
 import App from './App';
+import '@testing-library/jest-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders learn react link', async () => {
+  await act(async () => {
+    render(<App />);
+  });
+  expect(screen.getByText("designguy-radio-app gnfbufgnk")).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      designguy-radio-app
+      designguy-radio-app seyi
     </div>
   );
 }
