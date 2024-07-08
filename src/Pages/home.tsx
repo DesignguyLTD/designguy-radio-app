c742eaff6f68cb263f68c0451d24f17ef1370cb0
Mathetes041 <matthewonuoha41@gmail.com>
2024-07-09 00:15:52 +0100

    Fix: Review fixes

--- src/Pages/home.tsx
+++ src/Pages/home.tsx
@@ -1,37 +1,47 @@
-import React, { useContext } from "react";
+import { useEffect, useState } from "react";
+import Search from "../Components/Search";
+import RadioPlayer from "../playground/play";
+import styles from "../CSSModules/Home.module.css";
+import { playerInterface } from "../interface";
+import { PlayerContext } from "../Contexts/playerContext";
 
-import { AuthContext } from "../Contexts/authContext";
-import { Link } from "react-router-dom";
+function Home() {
+  let BASEURL = "https://radio.garden/api/ara/content/listen/";
+  const [station, setStation] = useState<playerInterface>({
+    _id: "string",
+    _score: 0,
+    _source: {
+      code: "string",
+      subtitle: "string",
+      type: "string",
+      title: "string",
+      stream: "string",
+      url: "string",
+    },
+  });
 
-const Home = () => {
-  const authContext = useContext(AuthContext);
+  const [currentUrl, setCurrentUrl] = useState("");
 
-  const handleLogout = () => {
-    authContext?.logout();
-  };
+  useEffect(() => {
+    const stream = station?._source.stream;
+    const url = station?._source.url;
+    const id = url?.split("/").pop(); // Extract the ID from the URL
+    const fullUrl = stream ? stream : `${BASEURL}${id}/channel.mp3`; // Construct the new URL
+    setCurrentUrl(fullUrl);
+  }, [station, BASEURL]);
+
   return (
-    <div>
-      home
-      <br />
-      <Link style={{ cursor: "pointer" }} to="/signup">
-        Sign Up
-      </Link>
-      <br />
-      {authContext?.isLoggedIn ? (
-        <div style={{ cursor: "pointer" }} onClick={handleLogout}>
-          Logout
-        </div>
-      ) : (
-        <Link style={{ cursor: "pointer" }} to="login">
-          Login
-        </Link>
-      )}
-      <br />
-      <Link to="play">
-        <button> P & P </button>
-      </Link>
-    </div>
+    <section className={styles.main_pg}>
+      <h1>
+        Bringing the world <br /> <span className={styles.br}>closer</span> to
+        you
+      </h1>
+      <PlayerContext.Provider value={{station, setStation}}>
+        <Search  />
+        <RadioPlayer streamUrl={currentUrl} />
+      </PlayerContext.Provider>
+    </section>
   );
-};
+}
 
 export default Home;
